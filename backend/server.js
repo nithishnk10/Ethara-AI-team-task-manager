53173624a7b7c68a9ab2316724c652a70c952445
Nithish Kumar <sivakumarnithish4@gmail.com>
2026-05-21 18:29:22 +0530

    Removed frontend serving from backend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
-const path = require("path");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
@@ -23,26 +22,7 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.log(err));
 
-
-
 const PORT = process.env.PORT || 5000;
-
-app.use(
-    express.static(
-        path.resolve(__dirname, "dist")
-    )
-);
-
-// React frontend fallback
-app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(
-        path.resolve(
-            __dirname,
-            "dist",
-            "index.html"
-        )
-    );
-});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
