90875a92a7caf7fc9b1422360284efb99800fc52
Nithish Kumar <sivakumarnithish4@gmail.com>
2026-05-21 16:16:13 +0530

    Integrated frontend with backend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
-
+const path = require("path");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
@@ -28,6 +28,11 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
