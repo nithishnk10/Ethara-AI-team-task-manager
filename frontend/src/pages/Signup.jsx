a5159a4704e923b3dc0567531ee0790309b7f1ad
Nithish Kumar <sivakumarnithish4@gmail.com>
2026-05-21 18:43:41 +0530

    Fixed toast import in signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { toast } from "react-toastify";
 import API from "../services/api";
 
 function Signup() {
